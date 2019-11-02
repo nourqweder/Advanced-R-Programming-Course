f67109a63ce49c249f99949842e504853b331c8c
Nour Elhouda Qweder <nouqw898@student.liu.se>
2019-11-02 10:48:24 +0100

    updtae

--- oop_output.R
+++ oop_output.R
@@ -11,12 +11,11 @@ longitudinaldata <- read.csv("./data/MIE.csv", sep = ",")
 summary(longitudinaldata)
 #You will need to design a class called “LongitudinalData” that characterizes the structure of this longitudinal dataset. You will also need to design classes to represent the concept of a “subject”, a “visit”, and a “room”.
 LongitudinalData <- setRefClass("LongitudinalData",
-                                fields = list(
-                                  id = "integer",
-                                  visit = "integer" ,
-                                  room = "character" ,
-                                  value = "numeric" , 
-                                  timpoint = "integer"
+                                fields = list(id = "integer", 
+                                              visit = "integer", 
+                                              room = "character", 
+                                              value = "numeric", 
+                                              timepoint = "integer"
                                 ),
                                 methods = list(
                                   #subject: a generic function for extracting subject-specific information
@@ -46,14 +45,49 @@ LongitudinalData <- setRefClass("LongitudinalData",
 #In addition you will need to implement the following functions
 
 #make_LD: a function that converts a data frame into a “LongitudinalData” object
-make_LD = function(obj){
+make_LD = function(x){
   LongitudinalData$new(
-    id = obj$id, 
-    visit = obj$visit, 
-    room = obj$room, 
-    value = obj$value, 
-    timepoint = obj$timepoint )
-}
+    id = x$id, 
+    visit = x$visit, 
+    room = x$room, 
+    value = x$value, 
+    timepoint = x$timepoint)
+} 
 
 
 
+###############################################################
+## Read in the data
+library(readr)
+library(magrittr)
+library(dplyr)
+
+longitudinaldata <- read.csv("data/MIE.csv", sep = ",")
+summary(longitudinaldata)
+x <- make_LD(longitudinaldata)
+class(x)
+print(class(x))
+print(x)
+
+## Subject 10 doesn't exist
+out <- subject(x, 10)
+print(out)
+
+out <- subject(x, 14)
+print(out)
+
+out <- subject(x, 54) %>% summary
+print(out)
+
+out <- subject(x, 14) %>% summary
+print(out)
+
+out <- subject(x, 44) %>% visit(0) %>% room("bedroom")
+print(out)
+
+## Show a summary of the pollutant values
+out <- subject(x, 44) %>% visit(0) %>% room("bedroom") %>% summary
+print(out)
+
+out <- subject(x, 44) %>% visit(1) %>% room("living room") %>% summary
+print(out)
